dfe39e544c82c336f766fa3d26d7e224a0ced7fe
one <2499899214@qq.com>
2016-06-21 15:03:40 +0800

    first file
    first file

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/***************************************************
+* @filename	main.cpp
+* @auther	yangenliang
+* @email	2499899214@qq.com
+* @data		2016-06-21 13:49:32.
+***************************************************/
+
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "onenet stared" << std::endl; 
+	return 0; 
+}
