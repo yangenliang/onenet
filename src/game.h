8584d202905e9936b810bd9bbd23d349eb4885c1
one <2499899214@qq.com>
2016-06-21 22:39:01 +0800

    folder
    folder

---
+++ src/game.h
@@ -0,0 +1,21 @@
+/***************************************************
+* @filename	game.h
+* @auther	yangenliang
+* @email	2499899214@qq.com
+* @data		2016-06-21 15:50:59.
+***************************************************/
+
+#ifndef _GAME_H_
+#define _GAME_H_
+
+class Game
+{
+public:
+	void start(); 
+	void end(); 
+
+private:
+	void main_loop(); 
+}
+
+#endif // _GAME_H_
